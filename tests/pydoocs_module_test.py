13d9784a8dded1b4aaddf4dbc1047e4a52d45cd5
Christopher Behrens <christopher.behrens@desy.de>
2019-04-23 12:56:32 +0200

    Import pydoocs in test case

--- tests/pydoocs_module_test.py
+++ tests/pydoocs_module_test.py
@@ -1,12 +1,14 @@
 """ Test module for the pydoocs module itself. """
 import unittest
 
+import pydoocs
+
 
 class PydoocsModuleTest(unittest.TestCase):
     """ Test class for the pydoocs module itself. """
 
     def test_members_existence(self):
-        pydoocs_members = [element for element in dir(self.pydoocs) if not element.startswith("__")]
+        pydoocs_members = [element for element in dir(pydoocs) if not element.startswith("__")]
         self.assertEqual(5, len(pydoocs_members))
         self.assertTrue('read' in pydoocs_members)
         self.assertTrue('write' in pydoocs_members)
@@ -15,18 +17,16 @@ class PydoocsModuleTest(unittest.TestCase):
         self.assertTrue('PyDoocsException' in pydoocs_members)
 
     def test_doc_strings(self):
-        """ Test all available pydoocs doc string. """
-        self.assertEqual(self.pydoocs.__doc__, "Python bindings to the standard DOOCS client API written in C/C++.")
-        self.assertEqual(self.pydoocs.read.__doc__, "Function to read data from a given DOOCS address.")
-        self.assertEqual(self.pydoocs.write.__doc__, "Function to write data to a given DOOCS address.")
-        self.assertEqual(self.pydoocs.names.__doc__, "Function to query names from a given DOOCS name domain.")
-        self.assertEqual(self.pydoocs.DoocsException.__doc__, "Custom DOOCS exception with "
+        self.assertEqual(pydoocs.__doc__, "Python bindings to the standard DOOCS client API written in C/C++.")
+        self.assertEqual(pydoocs.read.__doc__, "Function to read data from a given DOOCS address.")
+        self.assertEqual(pydoocs.write.__doc__, "Function to write data to a given DOOCS address.")
+        self.assertEqual(pydoocs.names.__doc__, "Function to query names from a given DOOCS name domain.")
+        self.assertEqual(pydoocs.DoocsException.__doc__, "Custom DOOCS exception with "
                                                               "dict_keys(['code', 'message']) in its first argument.")
-        self.assertEqual(self.pydoocs.PyDoocsException.__doc__, "Custom pydoocs exception with specific message.")
+        self.assertEqual(pydoocs.PyDoocsException.__doc__, "Custom pydoocs exception with specific message.")
 
     def test_version_attribute(self):
-        """ Test the version attribute. """
-        self.assertTrue('__version__' in dir(self.pydoocs))
-        self.assertEqual("2.0.4", self.pydoocs.__version__)
+        self.assertTrue('__version__' in dir(pydoocs))
+        self.assertEqual("2.0.4", pydoocs.__version__)
 
 
